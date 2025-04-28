f8be0dd0ebfb697534169a510e9b52cea1b1e6d5
yaya <punya.singka@gmail.com>
2025-04-28 15:29:39 +0700

    refactor

--- src/layouts/Allprojects.tsx
+++ src/layouts/Allprojects.tsx
@@ -1,18 +1,37 @@
 import { Link } from 'react-router-dom'
+interface Projects {
+  name: string
+  link: string
+}
+
+const defaultData: Projects[] = [
+  {
+    name: 'MeowList',
+    link: '/p1',
+  },
+  {
+    name: 'Todo',
+    link: '/p2',
+  },
+  {
+    name: 'MyFriends',
+    link: '/p3',
+  },
+]
 
 const Allprojects = () => {
   return (
     <div className="bg-amber-100">
       <ul className="p-2">
-        <Link to="/p1">
-          <li className="text-3xl p-2 hover:bg-amber-400">Project 1 : MeowList</li>
-        </Link>
-        <Link to="/p2">
-          <li className="text-3xl p-2 hover:bg-amber-400">Project 2 : Todo</li>
-        </Link>
-        <Link to="/p3">
-          <li className="text-3xl p-2 hover:bg-amber-400">Project 3 : MyFriends</li>
-        </Link>
+        {defaultData.map((e, index) => {
+          return (
+            <Link key={index} to={e.link}>
+              <li className="text-3xl p-2 hover:bg-amber-400">
+                Project {index + 1} : {e.name}
+              </li>
+            </Link>
+          )
+        })}
       </ul>
     </div>
   )

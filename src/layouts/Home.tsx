4adb91a66b5d98c62d47b13802c63422e0c23d24
miniBear <ax_zaa@windowslive.com>
2025-06-23 12:09:27 +0700

    test

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -9,7 +9,7 @@ for (let i = 1; i < nums.length; i++) {
 const Home = () => {
   return (
     <div>
-      <div>สวัสดี ฉันค่อย ๆ ปรากฏขึ้น...??? xxx</div>
+      <div>สวัสดี ฉันค่อย ๆ ปรากฏขึ้น...??? xxx llll</div>
     </div>
   )
 }

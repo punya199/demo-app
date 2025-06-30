0f541e3eef899e40162adec6a138683b9ebb4d27
miniBear <ax_zaa@windowslive.com>
2025-06-30 21:25:37 +0700

    Fix: edit server error when eslint error

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { analyzer } from 'vite-bundle-analyzer'
+import eslint from 'vite-plugin-eslint2'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), process.env.ANALYZE === 'true' && analyzer()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    eslint({
+      lintOnStart: true,
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['node_modules/**'],
+      cache: false,
+    }),
+    process.env.ANALYZE === 'true' && analyzer(),
+  ],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.webp'],
 })
